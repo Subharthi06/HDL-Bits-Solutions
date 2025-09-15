ae1c356d5748ec52defb4be902569a6cdd4a0876
SUBHARTHI ROY <m24eev017@iitj.ac.in>
2025-09-15 18:59:35 +0530

    Create 9_DFF2.v

---
+++ Circuits/2_Sequential_Logic/1_Latches&FFs/9_DFF2.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input d, 
+    input r,
+    output q);
+
+    always @(posedge clk)
+        begin
+            if(r)
+                q<=0;
+            else
+                q<=d;
+        end
+    
+endmodule
